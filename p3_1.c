94b3865daf54e7528650ab1e75cb2131b3a08453
parvej <parvej_about@outlook.com>
2020-04-12 06:45:49 -0400

    p3_1.c and p3_2.c added

---
+++ p3_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n = 10;
+    if(n>=0){
+        printf("The number is positive.\n");
+    }
+    else{
+        printf("Number is negative.\n");
+    }
+    return 0;
+}
