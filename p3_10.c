9f12e2cf7139fe5c9970cfebb5e085cbbbe475c7
parvej <parvej_about@outlook.com>
2020-04-13 21:52:57 -0400

    char

--- p3_10.c
+++ p3_10.c
@@ -5,7 +5,18 @@
 
 int main(void){
 
-    printf("Hello world.\n");
+    printf("Enter a char: ");
+    char ch;
+    scanf("%c", &ch);
+    if(ch >= 'a' && ch<= 'z')
+        printf("%c is small character\n", ch);
+    if(ch >= 'A' && ch<= 'Z')
+        printf("%c is capital character\n", ch);
+    else
+    {
+        printf("Not a character\n");
+    }
+    
     
     return 0;
 }
